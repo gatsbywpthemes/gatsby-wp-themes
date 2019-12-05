3148d7b070c0e25508fb4ea041f65c30b6d3638c
PeHaa <info@pehaa.com>
2019-12-05 11:34:32 +0100

    fix for lower/upper cases

--- packages/gatsby-theme-wp-ginger/wrap-root-element.js
+++ packages/gatsby-theme-wp-ginger/wrap-root-element.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 export default ({ element }, options) => {
-  if (options.fonts.some(el => el.indexOf(`fira sans`) > -1)) {
+  if (options.fonts.some(el => el.toLowerCase().indexOf(`fira sans`) > -1)) {
     require('typeface-fira-sans')
   }
-  if (options.fonts.some(el => el.indexOf(`abril fatface`) > -1)) {
+  if (
+    options.fonts.some(el => el.toLowerCase().indexOf(`abril fatface`) > -1)
+  ) {
     require('typeface-abril-fatface')
   }
 
