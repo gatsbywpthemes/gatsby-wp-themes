f99b2cb42667aa27f6f2c3a46ab0551a4ee42e96
PeHaa <info@pehaa.com>
2019-12-18 07:54:35 +0100

    testing fancybox

--- packages/gatsby-theme-wp-ginger/src/components/templates/Page.js
+++ packages/gatsby-theme-wp-ginger/src/components/templates/Page.js
@@ -5,10 +5,26 @@ import ParsedContent from '../../utils/ParsedContent'
 import SEO from '../seo/Seo'
 import pageStyles from '../../styles/page'
 import pageTitle from '../../styles/pageTitle'
+import { useLayoutEffect } from 'react'
+import jQuery from 'jquery'
+import '@fancyapps/fancybox'
 
 const Page = ({ page }) => {
   const { content } = page
   const ogType = page.isFrontPage ? 'website' : 'article'
+  useLayoutEffect(() => {
+    console.log(jQuery)
+    //const $ = jQuery
+    /* jQuery('.blocks-gallery-item a').fancybox({
+      
+    }) */
+    jQuery().fancybox({
+      selector: '.blocks-gallery-item a',
+      buttons: ['slideShow', 'zoom', 'fullScreen', 'close'],
+    })
+    // Options will go here
+  })
+
   return (
     <Layout useContainer={false}>
       <SEO
