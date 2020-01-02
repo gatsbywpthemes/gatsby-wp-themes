b7e8fa80fa0c5ce1271d07edcaf31fb3a64d302d
PeHaa <info@pehaa.com>
2020-01-02 16:31:20 +0100

    add new demo

---
+++ examples/ginger-2/config.js
@@ -0,0 +1,47 @@
+const config = {
+  wordPressUrl: `http://g-kitchen.test`,
+  // uploadsPath: `wp-content/uploads`,
+  // pathPrefix: "",
+  // postsPrefix: ``,
+  postsPath: `blog`,
+  // paginationPrefix: `page`,
+  postsPerPage: 5,
+  // addComments: true,
+  menuName: "main",
+  gingerWidgets: [`SocialFollow`, `RecentPosts`, `Tags`],
+  // siteUrl: "https://example.com",
+  // title: `Blog Title Placeholder`,
+  // author: `Name Placeholder`,
+  // description: `Description placeholder`,
+  social: [
+    {
+      name: `twitter`,
+      url: `https://twitter.com/gatsbyjs`,
+    },
+    {
+      name: `GitHub`,
+      url: `https://github.com/gatsbyjs`,
+    },
+    {
+      name: `Discord`,
+      url: `https://gatsby.dev/discord`,
+    },
+  ],
+  // twitterSummaryCardImage: `Gatsby_Monogram.png`,
+  fonts: ["Abril Fatface", "Lora:400,400i,700,700i"],
+  // gaTrackingId: 0,
+  // googleTagManagerId: 0,
+  // addSiteMap: false,
+  // siteMapOptions: {},
+  // addAlgoliaSearch: false,
+  // addColorModes: false,
+  // addFancyBox: true,
+  skipTitle: ["home"],
+}
+// https://gatsby-test-2.alexaspalato.website
+//https://gatsby-theme-demo.alexaspalato.website
+//`https://gatsby-theme-demo.alexaspalato.website`,
+// `https://gatsby-theme-demo.alexaspalato.website/wp-content/uploads/`,
+// `https://wp-pehaa.com/demo-pehaa1`,
+// `https://wp-pehaa.com/demo-pehaa1/wp-content/uploads/`,
+module.exports = config
