ae59ace578eb6b25cf21838f91e6d23d237414ff
PeHaa <info@pehaa.com>
2020-01-09 10:57:02 +0100

    add aria-label for accessibility

--- packages/gatsby-plugin-wordpress-fancybox/fancyBoxParserFunction.js
+++ packages/gatsby-plugin-wordpress-fancybox/fancyBoxParserFunction.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { domToReact } from 'html-react-parser'
+import React from "react"
+import { domToReact } from "html-react-parser"
 
-const ClientSideOnlyLazy = React.lazy(() => import('./FancyBoxWrapper'))
+const ClientSideOnlyLazy = React.lazy(() => import("./FancyBoxWrapper"))
 
 let counter = 0
 
 const findInnerA = node => {
   let value = null
-  if (node && node.name === 'a') {
+  if (node && node.name === "a") {
     return true
   }
 
@@ -20,13 +20,13 @@ const findInnerA = node => {
 const fancyBoxParserFunction = (node, { parserOptions }) => {
   const parserOptionsInner = {
     replace: domNode => {
-      if (domNode.name === 'a') {
+      if (domNode.name === "a") {
         if (
           domNode.attribs &&
           /\.(jpg|jpeg|gif|png)$/.test(domNode.attribs.href)
         ) {
           return (
-            <a {...domNode.attribs}>
+            <a aria-label="Open image in a lightbox" {...domNode.attribs}>
               {domToReact(domNode.children, parserOptions)}
             </a>
           )
@@ -34,14 +34,14 @@ const fancyBoxParserFunction = (node, { parserOptions }) => {
           return <>{domToReact(domNode.children, parserOptions)}</>
         }
       }
-    },
+    }
   }
-  const isSSR = typeof window === 'undefined'
+  const isSSR = typeof window === "undefined"
   if (
     node.name &&
     node.attribs &&
     node.attribs.class &&
-    node.attribs.class.indexOf('wp-block-gallery') > -1 &&
+    node.attribs.class.indexOf("wp-block-gallery") > -1 &&
     findInnerA(node)
   ) {
     counter = counter + 1
