647d754028f22c74b7957da08cfbc29ed5580f52
PeHaa <info@pehaa.com>
2020-04-30 09:55:48 +0200

    fix lint unused

--- examples/starter/configTypography.js
+++ examples/starter/configTypography.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import { rem } from "polished"
 
 const heading = {
--- examples/starter/src/gatsby-theme-wp-blog-starter/styles/grommet.js
+++ examples/starter/src/gatsby-theme-wp-blog-starter/styles/grommet.js
@@ -1,5 +1,6 @@
 import merge from "deepmerge"
 import grommetTheme from "gatsby-theme-wp-blog-starter/src/styles/grommet"
+// eslint-disable-next-line no-unused-vars
 import { colors } from "gatsby-theme-wp-blog-starter/src/gatsby-plugin-theme-ui/components"
 
 export default merge(grommetTheme, {
