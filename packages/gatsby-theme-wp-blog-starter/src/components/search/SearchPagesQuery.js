5e223590cbb0b97d7321e03c87b4fda1b9a16f85
PeHaa <info@pehaa.com>
2020-04-30 09:32:17 +0200

    fix lint unused

--- packages/gatsby-theme-wp-blog-starter/src/components/search/SearchPagesQuery.js
+++ packages/gatsby-theme-wp-blog-starter/src/components/search/SearchPagesQuery.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx, Flex, Box } from 'theme-ui'
+import { jsx, Flex } from 'theme-ui'
 import { useState } from 'react'
 import { Button } from 'grommet'
 import { useQuery } from '@apollo/react-hooks'
