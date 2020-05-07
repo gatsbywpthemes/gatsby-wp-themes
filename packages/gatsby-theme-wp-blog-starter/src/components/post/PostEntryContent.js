43ee4e4f7c96592fe45ecc8caa6292bd1aec5c11
PeHaa <info@pehaa.com>
2020-05-07 08:36:41 +0200

    fix [id] attributes on the page are not unique

--- packages/gatsby-theme-wp-blog-starter/src/components/post/PostEntryContent.js
+++ packages/gatsby-theme-wp-blog-starter/src/components/post/PostEntryContent.js
@@ -5,8 +5,9 @@ import gutenberg from '../../styles/theme-gutenberg'
 
 const PostEntryContent = ({ post, location, ...props }) => {
   const content = location === 'single' ? post.content : post.excerpt
+  const attributes = location === 'single' ? { id: 'content' } : {}
   return (
-    <Box id="content" sx={{ ...gutenberg }} {...props}>
+    <Box {...attributes} sx={{ ...gutenberg }} {...props}>
       <Box className="entry-content">
         <ParsedContent content={content} />
       </Box>
