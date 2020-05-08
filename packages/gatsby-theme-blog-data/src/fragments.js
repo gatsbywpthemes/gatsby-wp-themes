5ff5f28282f8b42fe637df6e5ec4b2d3bbe3d240
alexaspalato <alexaspalato@gmail.com>
2020-05-08 16:17:06 +0200

    separate fragments data

---
+++ packages/gatsby-theme-blog-data/src/fragments.js
@@ -0,0 +1,62 @@
+import { graphql } from 'gatsby'
+
+export const fragments = graphql`
+  fragment PostTemplateFragment on WpPost {
+    id
+    uri
+    slug
+    title
+    excerpt
+    date
+    postFormats {
+      taxonomyInfo {
+        name
+      }
+    }
+    featuredImage {
+      ...GatsbyImageQuery
+    }
+    categories {
+      nodes {
+        id
+        slug
+        name
+        uri
+      }
+    }
+    template {
+      ...PageTemplate
+    }
+    author {
+      name
+      slug
+      uri
+      avatar {
+        url
+      }
+    }
+    tags {
+      nodes {
+        name
+        slug
+        uri
+      }
+    }
+  }
+  fragment PageTemplate on WpContentTemplateUnion {
+    ... on WpDefaultTemplate {
+      templateName
+    }
+  }
+  fragment GatsbyImageQuery on WpMediaItem {
+    altText
+    sourceUrl
+    remoteFile {
+      childImageSharp {
+        fluid(maxWidth: 1200, quality: 80) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
--- packages/gatsby-theme-blog-data/src/templates/posts-query.js
+++ packages/gatsby-theme-blog-data/src/templates/posts-query.js
@@ -4,64 +4,6 @@ import Blog from '../components/Posts'
 export default Blog
 
 export const pageQuery = graphql`
-  fragment PostTemplateFragment on WpPost {
-    id
-    uri
-    slug
-    title
-    excerpt
-    date
-    postFormats {
-      taxonomyInfo {
-        name
-      }
-    }
-    featuredImage {
-      ...GatsbyImageQuery
-    }
-    categories {
-      nodes {
-        id
-        slug
-        name
-        uri
-      }
-    }
-    template {
-      ...PageTemplate
-    }
-    author {
-      name
-      slug
-      uri
-      avatar {
-        url
-      }
-    }
-    tags {
-      nodes {
-        name
-        slug
-        uri
-      }
-    }
-  }
-  fragment PageTemplate on WpContentTemplateUnion {
-    ... on WpDefaultTemplate {
-      templateName
-    }
-  }
-  fragment GatsbyImageQuery on WpMediaItem {
-    altText
-    sourceUrl
-    remoteFile {
-      childImageSharp {
-        fluid(maxWidth: 1200, quality: 80) {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-  }
   query GET_POSTS($skip: Int!, $limit: Int!) {
     allWpPost(limit: $limit, skip: $skip, sort: { order: DESC, fields: date }) {
       nodes {
