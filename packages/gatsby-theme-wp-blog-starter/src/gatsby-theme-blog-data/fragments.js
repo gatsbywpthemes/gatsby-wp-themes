8d841f8dd9393ef9bb312956cbf0a4553dee90a5
alexaspalato <alexaspalato@gmail.com>
2020-05-08 16:27:34 +0200

    fragments  starter

---
+++ packages/gatsby-theme-wp-blog-starter/src/gatsby-theme-blog-data/fragments.js
@@ -0,0 +1,72 @@
+import { graphql } from 'gatsby'
+
+export const fragments = graphql`
+  fragment ImageFluidFragment on WpMediaItem {
+    altText
+    sourceUrl
+    remoteFile {
+      childImageSharp {
+        fluid(maxWidth: 1200, maxHeight: 600, quality: 80) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+  fragment PostTemplateFragment_starter on WpPost {
+    id
+    uri
+    slug
+    title
+    excerpt
+    date
+    databaseId
+    postFormats {
+      taxonomyInfo {
+        name
+      }
+    }
+    template {
+      ...PageTemplates_starter
+    }
+    featuredImage {
+      ...ImageFluidFragment
+    }
+    categories {
+      nodes {
+        id
+        slug
+        name
+        uri
+      }
+    }
+    author {
+      name
+      slug
+      uri
+      avatar {
+        url
+      }
+    }
+    tags {
+      nodes {
+        name
+        slug
+        uri
+      }
+    }
+  }
+  fragment PageTemplates_starter on WpContentTemplateUnion {
+    ... on WpDefaultTemplate {
+      templateName
+    }
+    ... on WpFullWidthTemplate {
+      templateName
+    }
+    ... on WpLeftSidebarTemplate {
+      templateName
+    }
+    ... on WpRightSidebarTemplate {
+      templateName
+    }
+  }
+`
