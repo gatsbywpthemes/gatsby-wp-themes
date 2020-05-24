434b396f338fe15b5a528851ceaab8e10aca94e4
PeHaa <info@pehaa.com>
2020-05-24 13:59:04 +0200

    omg one more typo

--- examples/ginger/src/gatsby-theme-wp-ginger/utils/ActivatePageScripts.js
+++ examples/ginger/src/gatsby-theme-wp-ginger/utils/ActivatePageScripts.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react"
 import Prism from "prismjs"
 
-export const ActivatePageScripta = () => {
+export const ActivatePageScripts = () => {
   useEffect(() => {
     // call the highlightAll() function to style our code blocks
     Prism.highlightAll()
