b528a4c7b5309870fc309c6463f01cf2f1f8279c
PeHaa <info@pehaa.com>
2020-05-24 13:48:02 +0200

    fix and activate prism on pages

--- examples/ginger/src/gatsby-theme-wp-ginger/utils/ActivatePageScripts.js
+++ examples/ginger/src/gatsby-theme-wp-ginger/utils/ActivatePageScripts.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react"
 import Prism from "prismjs"
 
-export const ActivateScriptsPage = () => {
+export const ActivatePageScripta = () => {
   useEffect(() => {
     // call the highlightAll() function to style our code blocks
     Prism.highlightAll()
