2a880249604172d9508d1a9f571f6c4cf3ff180e
PeHaa <info@pehaa.com>
2020-05-30 16:06:18 +0200

    remove mapbox

--- examples/ginger-2/src/gatsby-theme-wp-ginger/utils/ParsedContent.js
+++ examples/ginger-2/src/gatsby-theme-wp-ginger/utils/ParsedContent.js
@@ -2,7 +2,6 @@ import React from "react"
 import ContentParser from "gatsby-plugin-wordpress-parser"
 import { cf7ParserFunction } from "gatsby-plugin-wpcf7"
 import fancyBoxParserFunction from "gatsby-plugin-wordpress-fancybox"
-import mapboxParserFunction from "gatsby-plugin-wp-mapbox"
 import { useThemeOptions } from "gatsby-theme-blog-data/src/hooks/useThemeOptions"
 
 const ParsedContent = ({ content }) => {
@@ -11,7 +10,6 @@ const ParsedContent = ({ content }) => {
   if (options.addFancyBox) {
     parserFunctions.push(fancyBoxParserFunction)
   }
-  parserFunctions.push(mapboxParserFunction)
   return <ContentParser content={content} customFn={parserFunctions} />
 }
 
