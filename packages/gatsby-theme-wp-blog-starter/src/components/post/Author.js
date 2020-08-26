c93c375bb460439f5b61ed0ab89b22d38af82069
alexaspalato <alexaspalato@gmail.com>
2020-08-26 15:38:18 +0200

    fix author

--- packages/gatsby-theme-wp-blog-starter/src/components/post/Author.js
+++ packages/gatsby-theme-wp-blog-starter/src/components/post/Author.js
@@ -3,7 +3,7 @@ import { jsx } from 'theme-ui'
 import { Link } from 'gatsby'
 
 export const Author = ({ post, ...props }) => {
-  const { name, slug } = post.author
+  const { name, slug } = post.author.node
   return (
     <Link aria-label={`visit ${name} page`} to={`/author/${slug}`} {...props}>
       {name}
