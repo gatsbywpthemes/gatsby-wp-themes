8aeb1c7172146086e49deebd59c6961606423268
alexaspalato <alexaspalato@gmail.com>
2020-11-09 08:25:35 +0100

    full width page

--- examples/starter-chakra/src/components/Layout.js
+++ examples/starter-chakra/src/components/Layout.js
@@ -1,20 +1,46 @@
+/** @jsx jsx */
 import React from 'react'
 import { Header } from './header'
 import { Footer } from './footer'
-import { useTheme, ChakraProvider, Box } from '@chakra-ui/core'
+import { useTheme, ChakraProvider, Box, jsx } from '@chakra-ui/core'
 import customTheme from '../gatsby-plugin-chakra-ui/theme'
-// import 'normalize.css'
-// import '../styles/globalstyles.scss'
+import Headroom from 'react-headroom'
 import '../styles/wp-styles/styles.scss'
 
-export const Layout = ({ children }) => {
+export const Layout = ({ children, page, type = 'page' }) => {
+  const layoutClass = page !== undefined ? (page.slug ? page.slug : page) : ''
+  const pageTemplate = page && page.template ? page.template.__typename : ''
+
+  const fullWidthClass =
+    pageTemplate === 'WpFullWidthTemplate' ? 'fullWidth' : ''
   return (
-    <>
-      <Header />
-      <Box as="main" py={16}>
+    <Box
+      sx={{
+        '&.fullWidth': {
+          '.mainContainer': {
+            maxWidth: `100%`,
+            px: 0,
+          },
+        },
+      }}
+      className={`${layoutClass}-${type} ${fullWidthClass}`}
+    >
+      <Headroom>
+        <Header />
+      </Headroom>
+      <Box
+        as="main"
+        py={16}
+        sx={{
+          '.fullWidth &': {
+            py: 0,
+            // mt: '-32px',
+          },
+        }}
+      >
         {children}
       </Box>
       <Footer />
-    </>
+    </Box>
   )
 }
