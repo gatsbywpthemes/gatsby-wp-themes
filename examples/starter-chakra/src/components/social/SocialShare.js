8020dfb459a09ad41c17e317f373cc1564851a19
alexaspalato <alexaspalato@gmail.com>
2020-11-10 08:15:18 +0100

    social share

--- examples/starter-chakra/src/components/social/SocialShare.js
+++ examples/starter-chakra/src/components/social/SocialShare.js
@@ -1,32 +1,37 @@
+/** @jsx jsx */
+import { jsx, Center, Heading, IconButton, HStack } from '@chakra-ui/core'
 import React from 'react'
 import { Twitter, Facebook, Pinterest } from './icons'
 import { FiShare2 } from 'react-icons/fi'
 
 export const SocialShare = ({ url, title, media }) => {
   return (
-    <>
-      <h3>Share on social media</h3>
-      <span>
-        <FiShare2 />
-      </span>
+    <Center flexDirection="column" mt={10}>
+      <Heading as="h3" fontSize="md" textTransform="uppercase">
+        Share on social media
+      </Heading>
+      <IconButton
+        isRound={true}
+        variant="outline"
+        colorScheme="gray"
+        size="sm"
+        cursor="auto"
+        icon={<FiShare2 />}
+      />
 
-      <ul>
-        <li>
-          <Twitter url={url} title={title}>
-            Twitter
-          </Twitter>
-        </li>
-        <li>
-          <Facebook url={url} quote={title}>
-            Facebook
-          </Facebook>
-        </li>
-        <li>
-          <Pinterest url={url} media={media}>
-            Pinterest
-          </Pinterest>
-        </li>
-      </ul>
-    </>
+      <HStack fontWeight="bold" fontSize="sm" mt={4}>
+        <Twitter url={url} title={title}>
+          Twitter
+        </Twitter>
+        <span>/</span>
+        <Facebook url={url} quote={title}>
+          Facebook
+        </Facebook>
+        <span>/</span>
+        <Pinterest url={url} media={media}>
+          Pinterest
+        </Pinterest>
+      </HStack>
+    </Center>
   )
 }
