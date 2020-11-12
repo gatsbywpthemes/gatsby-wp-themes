af94fe392551cb9ad6273d2777170fcae2234f8f
alexaspalato <alexaspalato@gmail.com>
2020-11-12 06:43:39 +0100

    archiveTitle

--- packages/gatsby-theme-wp-starter/src/components/archive/ArchiveTitle.js
+++ packages/gatsby-theme-wp-starter/src/components/archive/ArchiveTitle.js
@@ -1,11 +1,26 @@
-import React from 'react'
+/** @jsx jsx */
+import {
+  jsx,
+  Box,
+  Heading,
+  useColorModeValue as colorMode,
+} from '@chakra-ui/core'
 
 export const ArchiveTitle = ({ name, text }) => {
   return (
-    <div className="archive-title">
-      <h1>
+    <Box
+      px={[4, 10]}
+      py={2}
+      mb={10}
+      borderRadius="lg"
+      display="inline-block"
+      bg={colorMode('gray.200', 'gray.900')}
+      className="archive-title"
+      boxShadow="md"
+    >
+      <Heading fontSize="2xl" textTransform="uppercase">
         {text} {name}
-      </h1>
-    </div>
+      </Heading>
+    </Box>
   )
 }
