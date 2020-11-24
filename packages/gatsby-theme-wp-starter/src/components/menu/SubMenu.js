2c218a25df00b8bb914f73d2f467a491b33fce6d
alexaspalato <alexaspalato@gmail.com>
2020-11-24 19:23:32 +0100

    menu working

--- packages/gatsby-theme-wp-starter/src/components/menu/SubMenu.js
+++ packages/gatsby-theme-wp-starter/src/components/menu/SubMenu.js
@@ -1,7 +1,60 @@
 import React from 'react'
-import { useDisclosure, SlideFade, chakra } from '@chakra-ui/react'
+import {
+  useDisclosure,
+  SlideFade,
+  chakra,
+  Menu,
+  MenuButton,
+  MenuItem as SubMenuItem,
+  MenuList,
+  Button,
+} from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons'
+import { FaChevronDown } from 'react-icons/fa'
+
 import { Collapse } from 'starterUiComponents'
 import { MenuLink, MenuItem } from 'starterComponents'
+
+const SubMenuV = ({ menuItem }) => {
+  return (
+    <chakra.li
+      className="has-subMenu menu-item"
+      position="relative"
+      key={menuItem.id}
+    >
+      <Collapse menuItem={menuItem}>
+        <chakra.ul className="menuItemGroup sub-menu">
+          {menuItem.children.map((item) => (
+            <MenuItem key={item.id} menuItem={item} />
+          ))}
+        </chakra.ul>
+      </Collapse>
+    </chakra.li>
+  )
+}
+
+const SubMenuH = ({ menuItem }) => {
+  console.log('item', menuItem)
+
+  return (
+    <Menu key={menuItem.id}>
+      <MenuButton
+        as={Button}
+        variant="unstyled"
+        rightIcon={<ChevronDownIcon />}
+      >
+        {menuItem.label}
+      </MenuButton>
+      <MenuList>
+        {menuItem.children.map((item) => (
+          <SubMenuItem as="li" key={item.id}>
+            <MenuLink menuItem={item} />
+          </SubMenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  )
+}
 
 export const SubMenu = ({ menuItem, orientation }) => {
   const { isOpen, onOpen, onClose, onToggle } = useDisclosure()
@@ -56,29 +109,9 @@ export const SubMenu = ({ menuItem, orientation }) => {
     },
   }
 
-  return (
-    <chakra.li
-      className="has-subMenu menu-item"
-      position="relative"
-      // onMouseEnter={orientation === 'H' && onOpen}
-      onClickOutside={orientation === 'H' && onClose}
-      onClick={orientation === 'H' && onToggle}
-      // _after={orientation === 'H' && { content: "'>'" }}
-      key={menuItem.id}
-    >
-      <WithCollapse orientation={orientation} menuItem={menuItem}>
-        {orientation === 'H' && <MenuLink menuItem={menuItem} />}
-        <WithSlideFade orientation={orientation} isOpen={isOpen}>
-          <chakra.ul
-            className="menuItemGroup sub-menu"
-            sx={orientation === 'H' && { ...subMenuStyles }}
-          >
-            {menuItem.children.map((item) => (
-              <MenuItem key={item.id} menuItem={item} />
-            ))}
-          </chakra.ul>
-        </WithSlideFade>
-      </WithCollapse>
-    </chakra.li>
+  return orientation === 'H' ? (
+    <SubMenuH menuItem={menuItem} />
+  ) : (
+    <SubMenuV menuItem={menuItem} />
   )
 }
