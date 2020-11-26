79ab1eb0c0e3c5e26a84dfc75116087da0673e4e
Alexandra Spalato <alexaspalato@gmail.com>
2020-11-26 06:36:45 +0100

    post nav styles

--- packages/gatsby-theme-wp-starter/src/components/post/PrevNextPostNavigation.js
+++ packages/gatsby-theme-wp-starter/src/components/post/PrevNextPostNavigation.js
@@ -5,10 +5,9 @@ import { Link } from 'gatsby'
 const LinkButton = (props) => (
   <Button
     as={Link}
-    variant="ghost"
+    variant="unstyled"
     size="xs"
     className="prev"
-    colorScheme="red"
     textTransform="uppercase"
     letterSpacing="widest"
     {...props}
@@ -23,7 +22,12 @@ const renderPreviousLink = ({ prev }) => {
   }
 
   return (
-    <LinkButton to={previousLink} aria-label="visit previous post">
+    <LinkButton
+      className="previous"
+      sx={{ ...linkStyles }}
+      to={previousLink}
+      aria-label="visit previous post"
+    >
       <span>Previous</span>
     </LinkButton>
   )
@@ -32,7 +36,12 @@ const renderPreviousLink = ({ prev }) => {
 const renderNextLink = ({ next }) => {
   if (next) {
     return (
-      <LinkButton aria-label="visit next post" to={`${next}`}>
+      <LinkButton
+        className="next"
+        sx={{ ...linkStyles }}
+        aria-label="visit next post"
+        to={`${next}`}
+      >
         <span>Next</span>
       </LinkButton>
     )
@@ -48,4 +57,35 @@ export const PrevNextPostNavigation = ({ ctx, style }) => {
       {renderNextLink(ctx)}
     </Flex>
   )
+}
+
+const linkStyles = {
+  transition: '.6s',
+  fontWeight: 'bold',
+  '&.next': {
+    transform: 'translate3d(1.75rem, 0, 0)',
+    mr: [4, 0],
+  },
+  '&.previous': {
+    transform: 'translate3d(-1.75rem, 0, 0)',
+    ml: [4, 0],
+  },
+  ':hover': {
+    transform: 'translate3d(0, 0, 0)',
+    textDecoration: 'none',
+  },
+  '&.previous::before,&.next::after': {
+    content: '""',
+    width: '1.5rem',
+    height: '1px',
+    mr: '0.25rem',
+    bg: 'currentColor',
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    transition: '0.6s',
+    transform: 'scaleX(0)',
+  },
+  ':hover::before, :hover::after': {
+    transform: 'scaleX(1)',
+  },
 }
