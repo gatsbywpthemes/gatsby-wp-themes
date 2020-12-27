f5d64202937ca0634d94e61d8f9560188cb20ddb
Alexandra Spalato <alexaspalato@gmail.com>
2020-12-27 10:02:07 +0100

    404 page

--- packages/gatsby-theme-wp-starter/src/pages/404.js
+++ packages/gatsby-theme-wp-starter/src/pages/404.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Layout } from '../components'
 import { Seo } from '@gatsbywpthemes/gatsby-plugin-wp-seo'
+import { Container } from 'starterUiComponents'
+import { chakra } from '@chakra-ui/react'
 
 const NotFoundPage = ({ location }) => (
   <Layout location={{ location }} page="404">
     <Seo title="404: Not found" />
     <article>
-      <h1>404</h1>
-      <div className="entry-content">
-        <p>That page can't be found.</p>
-        <p>It looks like nothing was found at this location.</p>
-      </div>
+      <Container className="text-center">
+        <chakra.h1
+          border="10px double"
+          borderColor="primary"
+          display="inline-block"
+          className="px-10 py-6 mb-10"
+        >
+          404
+        </chakra.h1>
+        <div className="text-sm font-bold tracking-wider uppercase entry-content">
+          <p>That page can't be found.</p>
+          <p>It looks like nothing was found at this location.</p>
+        </div>
+      </Container>
     </article>
   </Layout>
 )
