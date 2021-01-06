8be1751dcd512774ed74539127f3942fc91938f3
PeHaa <info@pehaa.com>
2021-01-06 10:49:45 +0100

    remove unnecessary props

--- packages/gatsby-theme-wp-ginger-chakra/src/components/search/SearchQueries.js
+++ packages/gatsby-theme-wp-ginger-chakra/src/components/search/SearchQueries.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { SearchPostsQuery, SearchPagesQuery } from 'gingerThemeComponents'
 import { Box } from '@chakra-ui/react'
 
-export const SearchQueries = ({ search }) => {
+export const SearchQueries = () => {
   return (
     <Box className="search-results" mb="6">
       <SearchPostsQuery />
