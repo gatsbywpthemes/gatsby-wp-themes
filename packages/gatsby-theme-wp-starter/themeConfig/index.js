e5281964a5d7e6bc5cf61ff4ddde39257db656ba
Alexandra Spalato <alexaspalato@gmail.com>
2021-01-08 12:23:25 +0100

    theme config fontsize

--- packages/gatsby-theme-wp-starter/themeConfig/index.js
+++ packages/gatsby-theme-wp-starter/themeConfig/index.js
@@ -21,26 +21,6 @@ module.exports = {
     ...defaultTheme.boxShadow,
   },
 
-  // widths: {
-  //   none: 'none',
-  //   0: '0rem',
-  //   xs: '20rem',
-  //   sm: '24rem',
-  //   md: '28rem',
-  //   lg: '32rem',
-  //   xl: '36rem',
-  //   '2xl': '42rem',
-  //   '3xl': '48rem',
-  //   '4xl': '56rem',
-  //   '5xl': '64rem',
-  //   '6xl': '72rem',
-  //   '7xl': '80rem',
-  //   full: '100%',
-  //   min: 'min-content',
-  //   max: 'max-content',
-  //   prose: '65ch',
-  // },
-
   //Typography
   letterSpacing: {
     ...defaultTheme.letterSpacing,
