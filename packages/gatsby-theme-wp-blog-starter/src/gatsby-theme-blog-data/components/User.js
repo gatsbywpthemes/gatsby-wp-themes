88ae5e66e5aa6d73a549cb00d37024c622a802d4
PeHaa <info@pehaa.com>
2021-01-13 14:05:33 +0100

    remove packages/gatsby-theme-wp-blog-starter

--- packages/gatsby-theme-wp-blog-starter/src/gatsby-theme-blog-data/components/User.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import User from '../../components/templates/User'
-
-export default ({ data, pageContext }) => {
-  return (
-    <User user={{ ...data.wpUser, posts: data.allWpPost }} ctx={pageContext} />
-  )
-}
