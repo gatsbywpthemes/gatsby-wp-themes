346149ba68800856097a8b3055589ac65811752a
PeHaa <info@pehaa.com>
2021-01-13 14:02:12 +0100

    remove examples/starter-1

--- examples/starter-1/src/gatsby-theme-wp-blog-starter/components/Layout.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import { Layout as StyledLayout } from "gatsby-theme-wp-blog-starter/src/components/Layout"
-import "../../styles/style.scss"
-
-export const Layout = props => <StyledLayout {...props} />
--- examples/starter-1/src/gatsby-theme-wp-blog-starter/styles/articleStyles.js
+++
@@ -1,8 +0,0 @@
-import merge from "deepmerge"
-import { articleStyles as styles } from "gatsby-theme-wp-blog-starter/src/styles/articleStyles"
-
-export const articleStyles = merge(styles, {
-  ".entry-title": {
-    fontSize: ["l", "xl"],
-  },
-})
