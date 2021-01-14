3e6e0a692fa86ef4a9509acebff0702bda852112
Alexandra Spalato <alexaspalato@gmail.com>
2021-01-14 16:23:33 +0100

    starter no styles

--- examples/starter-data-nostyles/src/@gatsbywpthemes/gatsby-theme-blog-data/components/Posts.js
+++ examples/starter-data-nostyles/src/@gatsbywpthemes/gatsby-theme-blog-data/components/Posts.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Posts from '../../components/templates/Posts'
+import Posts from '../../../components/templates/Posts'
 
 export default ({ data, pageContext }) => {
   return <Posts posts={data?.allWpPost} ctx={pageContext} />
