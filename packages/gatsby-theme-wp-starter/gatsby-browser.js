afae0d43a06a44fb22f8da337a5fc5259d01541c
PeHaa <info@pehaa.com>
2021-02-16 14:41:18 +0100

    colors from wp

--- packages/gatsby-theme-wp-starter/gatsby-browser.js
+++ packages/gatsby-theme-wp-starter/gatsby-browser.js
@@ -1,1 +1,8 @@
 import './src/styles/tailwind.css'
+import React from 'react'
+
+import { CustomThemeWrapper } from './src/components/CustomThemeWrapper'
+console.log(CustomThemeWrapper)
+export const wrapPageElement = ({ element }) => (
+  <CustomThemeWrapper>{element}</CustomThemeWrapper>
+)
