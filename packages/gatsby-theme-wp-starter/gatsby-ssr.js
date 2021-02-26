2166127124c9119120a2ad1db30553c704b0fd6d
PeHaa <info@pehaa.com>
2021-02-26 16:10:48 +0100

    add custom theme wrapper to gatsby-ssr

--- packages/gatsby-theme-wp-starter/gatsby-ssr.js
+++ packages/gatsby-theme-wp-starter/gatsby-ssr.js
@@ -1,1 +1,7 @@
+import React from 'react'
 
+import { CustomThemeWrapper } from './src/components/CustomThemeWrapper'
+console.log(CustomThemeWrapper)
+export const wrapPageElement = ({ element }) => (
+  <CustomThemeWrapper>{element}</CustomThemeWrapper>
+)
