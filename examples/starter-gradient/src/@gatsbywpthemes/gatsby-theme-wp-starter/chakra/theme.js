7b415d93260f40e159bd33ed86337c21e679f6fe
PeHaa <info@pehaa.com>
2021-02-28 14:00:28 +0100

    fixed (2) shadowing chakra components

--- examples/starter-gradient/src/@gatsbywpthemes/gatsby-theme-wp-starter/chakra/theme.js
+++ examples/starter-gradient/src/@gatsbywpthemes/gatsby-theme-wp-starter/chakra/theme.js
@@ -1,10 +1,8 @@
 import { extendTheme } from "@chakra-ui/react"
-
 import themeBase from "@gatsbywpthemes/gatsby-theme-wp-starter/src/chakra/theme"
-import baseComponents from "@gatsbywpthemes/gatsby-theme-wp-starter/src/chakra/components"
 import colorsSettings from "../../../../configColors"
 import typographySettings from "../../../../configTypography"
-import components from "./components"
+import components from "./shadow/components"
 import textStyles from "./textStyles"
 import layerStyles from "./layerStyles"
 import styles from "./styles"
@@ -12,7 +10,7 @@ import styles from "./styles"
 const theme = extendTheme({
   ...themeBase,
   components: {
-    ...baseComponents,
+    ...themeBase.components,
     ...components,
   },
   colors: colorsSettings,
