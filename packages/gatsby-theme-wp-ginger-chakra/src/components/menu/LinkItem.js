ddf2f70864ae5da1fb5d07f1877ca94ce01124db
PeHaa <info@pehaa.com>
2021-04-01 15:32:05 +0200

    fix ginger bugs

--- packages/gatsby-theme-wp-ginger-chakra/src/components/menu/LinkItem.js
+++ packages/gatsby-theme-wp-ginger-chakra/src/components/menu/LinkItem.js
@@ -9,19 +9,22 @@ export const LinkItem = ({ menuItem }) => {
   const { wordPressUrl } = useThemeOptions()
   let url = menuItem.url
 
-  if (url.startsWith('https://') || url.startsWith('http://')) {
+  if (
+    (!url.includes(wordPressUrl) && url.startsWith('https://')) ||
+    url.startsWith('http://')
+  ) {
     const targetRelAttrs =
       menuItem.target === '_blank'
         ? { target: '_blank', rel: 'noopener noreferrer' }
         : {}
     return (
-      <chakra.a textStyle="specialLink" href={menuItem.url} {...targetRelAttrs}>
+      <chakra.a textStyle="specialLink" href={url} {...targetRelAttrs}>
         {menuItem.label}
       </chakra.a>
     )
   } else {
-    return menuItem.url !== '#' ? (
-      menuItem.url === wordPressUrl ? (
+    return url !== '#' ? (
+      url === wordPressUrl ? (
         <Box
           as={GatsbyLink}
           textStyle="specialLink"
@@ -33,7 +36,7 @@ export const LinkItem = ({ menuItem }) => {
           as={GatsbyLink}
           textStyle="specialLink"
           fontWeight="bold"
-          to={createLocalLink(menuItem.url, slashes(wordPressUrl))}
+          to={createLocalLink(url, slashes(wordPressUrl))}
           dangerouslySetInnerHTML={{ __html: menuItem.label }}
         />
       )
