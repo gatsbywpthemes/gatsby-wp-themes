5de4d5c6659b8892d2608963988735e29c1bc778
PeHaa <info@pehaa.com>
2021-04-10 13:42:15 +0200

    fix accidental character

--- examples/starter-base-dev/gatsby-ssr.js
+++ examples/starter-base-dev/gatsby-ssr.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import { CustomThemeWrapper } from "./src/components/CustomThemeWrapper"
-c
+
 export const wrapPageElement = ({ element }) => (
   <CustomThemeWrapper>{element}</CustomThemeWrapper>
 )
