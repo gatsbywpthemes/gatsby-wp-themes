97e8b739281748840f69e958b20b1b571b494653
PeHaa <info@pehaa.com>
2021-04-14 17:13:07 +0200

    border to currentcolor in form

--- packages/gatsby-theme-wp-base/src/chakra/components/Input.js
+++ packages/gatsby-theme-wp-base/src/chakra/components/Input.js
@@ -3,10 +3,10 @@ const Input = {
     background: 'transparent',
     borderWidth: 0,
     borderBottomWidth: '1px',
+    borderColor: 'currentcolor',
     boxShadow: 'currentcolor 0px 0px 0px inset',
     transition: '0.6s',
     px: 0,
-
     borderRadius: 0,
     WebkitAppearance: 'none',
     '&:focus': {
