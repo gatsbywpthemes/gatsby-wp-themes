4d51270570edb14a3e0e11e43bbae96ea129fc46
PeHaa <info@pehaa.com>
2021-04-19 10:47:01 +0200

    fix runtime error if form w/o class

--- packages/gatsby-plugin-wpcf7/src/cf7ParserFunction.js
+++ packages/gatsby-plugin-wpcf7/src/cf7ParserFunction.js
@@ -1,8 +1,12 @@
 import React from "react"
 import ContactForm7 from "./ContactForm7"
 
-export const cf7ParserFunction = node => {
-  if (node.name === "form" && node.attribs.class.indexOf("wpcf7-form") > -1) {
+export const cf7ParserFunction = (node) => {
+  if (
+    node.name === "form" &&
+    node.attribs.class &&
+    node.attribs.class.indexOf("wpcf7-form") > -1
+  ) {
     return <ContactForm7 formObject={node} />
   }
 }
