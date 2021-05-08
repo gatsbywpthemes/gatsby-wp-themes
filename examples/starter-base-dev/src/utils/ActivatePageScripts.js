df54220cbede55f56379b87a86afd688a81c8613
PeHaa <info@pehaa.com>
2021-05-08 18:22:40 +0200

    fix starter-base-dev missing Prism highlighting

--- examples/starter-base-dev/src/utils/ActivatePageScripts.js
+++ examples/starter-base-dev/src/utils/ActivatePageScripts.js
@@ -1,3 +1,10 @@
-import React from 'react'
+import { useEffect } from "react"
+import Prism from "prismjs"
 
-export const ActivatePageScripts = () => <></>
+export const ActivatePageScripts = () => {
+  useEffect(() => {
+    // call the highlightAll() function to style our code blocks
+    Prism.highlightAll()
+  }, [])
+  return null
+}
--- examples/starter-base-dev/src/utils/ActivatePostScripts.js
+++ examples/starter-base-dev/src/utils/ActivatePostScripts.js
@@ -1,3 +1,10 @@
-import React from 'react'
+import { useEffect } from "react"
+import Prism from "prismjs"
 
-export const ActivatePostScripts = () => <></>
+export const ActivatePostScripts = () => {
+  useEffect(() => {
+    // call the highlightAll() function to style our code blocks
+    Prism.highlightAll()
+  }, [])
+  return null
+}
