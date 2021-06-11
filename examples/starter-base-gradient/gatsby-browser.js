9484ed2b6afb936416ce20cedc800f72ff9ad9b7
PeHaa <info@pehaa.com>
2021-06-11 12:04:34 +0200

    remove unused files and console.log

--- examples/starter-base-gradient/gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-// import React from "react"
-
-// import { CustomThemeWrapper } from "baseComponents/CustomThemeWrapper"
-// console.log(CustomThemeWrapper)
-// export const wrapPageElement = ({ element }) => (
-//   <CustomThemeWrapper>{element}</CustomThemeWrapper>
-// )
