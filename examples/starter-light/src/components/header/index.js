55e3d6d9db4ecb97e2e63d17325184768783bdef
Alexandra Spalato <alexaspalato@gmail.com>
2021-09-26 13:50:27 +0200

    submenu

--- examples/starter-light/src/components/header/index.js
+++ examples/starter-light/src/components/header/index.js
@@ -4,7 +4,7 @@ import { Branding } from "./Branding"
 
 export const Header = ({ ...props }) => {
   return (
-    <header className={`py-4 shadow-md`} {...props}>
+    <header className={`py-4 shadow-md relative`} {...props}>
       <div className={`flex justify-between center-container`}>
         <Branding title="Starter Light" />
         <MainMenu orientation="H" />
--- examples/starter-light/src/components/menu/Submenu.js
+++ examples/starter-light/src/components/menu/Submenu.js
@@ -1,13 +1,14 @@
-import React from "react"
+import React, { Fragment, useEffect, useRef, useState } from "react"
 import { CgChevronDown } from "react-icons/cg"
 import { Collapse } from "../ui-components"
-import { Menu } from "@headlessui/react"
+import { Menu, Transition } from "@headlessui/react"
 import { MenuLink } from "./MenuLink"
 import { MenuItem } from "./MenuItem"
+import clsx from "clsx"
 
 const SubmenuV = ({ menuItem }) => {
   return (
-    <div className={`relative has-submenu menu-item`} key={menuItem.id}>
+    <div className={`relative has-submenu menu-item `}>
       <Collapse trigger={menuItem.label}>
         <ul className="flex menuItemGroup sub-menu">
           {menuItem.children.map((item) => (
@@ -21,19 +22,40 @@ const SubmenuV = ({ menuItem }) => {
 
 const SubmenuH = ({ menuItem }) => {
   return (
-    <Menu key={menuItem.id}>
-      <Menu.Button className="menu-item hover:text-accentColor dark:hover:text-dark-accentColor">
-        {menuItem.label}
-        <CgChevronDown />
-      </Menu.Button>
-
-      <Menu.Items>
-        {menuItem.children.map((item) => (
-          <Menu.Item key={item.id}>
-            {({ active }) => <MenuLink menuItem={item} />}
-          </Menu.Item>
-        ))}
-      </Menu.Items>
+    <Menu as="div" className={`relative inline-block`}>
+      <div>
+        <Menu.Button
+          className={clsx(
+            `inline-flex w-auto justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500`
+          )}
+        >
+          {menuItem.label}
+          <CgChevronDown className={`-mr-1 ml-2 h-5 w-5`} aria-hidden="true" />
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-300"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items
+          className={clsx(
+            `origin-top-right absolute  mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none`
+          )}
+        >
+          {menuItem.children.map((item) => (
+            <div className="py-1">
+              <Menu.Item key={item.id}>
+                {({ active }) => <MenuLink menuItem={item} />}
+              </Menu.Item>
+            </div>
+          ))}
+        </Menu.Items>
+      </Transition>
     </Menu>
   )
 }
