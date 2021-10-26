046812e1f6334ead927dec4b3cd8099627876ba1
Alexandra Spalato <alexaspalato@gmail.com>
2021-10-26 16:30:40 +0200

    sidebar

--- examples/starter-naked/src/components/archive/index.js
+++ examples/starter-naked/src/components/archive/index.js
@@ -13,17 +13,34 @@ export const ArchiveContent = ({ posts, ctx, name, description }) => {
     layoutWidth,
     widgetAreas: { sidebarWidgets },
   } = useThemeOptions()
+
+  const hasSidebar = sidebarWidgets && archiveSidebar
   return (
-    <>
+    <div
+      className={`mainContainer  ${
+        hasSidebar ? `max-w-xl` : `max-w-${layoutWidth.archive}`
+      }`}
+    >
       {name && <Archivetitle name={name} text="Posts for " />}
       <Description description={description} />
-      <PostsList posts={posts} className="space-y-10" />
-      {archiveSidebar && (
-        <div className={clsx("xl:col-span-1 col-span-3")}>
-          <Sidebar widgets={sidebarWidgets} />
-        </div>
-      )}
+      <div
+        className={`${
+          hasSidebar && "lg:grid"
+        } xl:grid-cols-3 grid-cols-10 gap-8`}
+      >
+        <PostsList
+          posts={posts}
+          className={clsx("xl:col-span-2 col-span-7", {
+            "order-2": archiveSidebar === "left",
+          })}
+        />
+        {archiveSidebar && (
+          <div className={clsx("xl:col-span-1 col-span-3")}>
+            <Sidebar widgets={sidebarWidgets} />
+          </div>
+        )}
+      </div>
       <Pagination ctx={ctx} />
-    </>
+    </div>
   )
 }
