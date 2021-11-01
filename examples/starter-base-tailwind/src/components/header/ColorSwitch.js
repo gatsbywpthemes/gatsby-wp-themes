95736def51366d74751a97cc538c894580b787ce
Alexandra Spalato <alexaspalato@gmail.com>
2021-11-01 11:27:13 +0100

    use dark mode

--- examples/starter-base-tailwind/src/components/header/ColorSwitch.js
+++ examples/starter-base-tailwind/src/components/header/ColorSwitch.js
@@ -1,30 +1,18 @@
 import React from "react"
-// import { ThemeToggler } from "gatsby-plugin-dark-mode"
 import { HiOutlineSun as Sun, HiOutlineMoon as Moon } from "react-icons/hi"
 import useDarkMode from "use-dark-mode"
+import Helmet from "react-helmet"
 
 export const ColorSwitch = (props) => {
   const darkMode = useDarkMode(false)
-  console.log("dark", darkMode)
+
   return (
-    // <ThemeToggler>
-    //   {({ theme, toggleTheme }) => (
-    //     <label {...props}>
-    //       <input
-    //         type="checkbox"
-    //         onChange={(e) => toggleTheme(e.target.checked ? "dark" : "light")}
-    //         checked={theme === "dark"}
-    //         className="hidden"
-    //       />{" "}
-    //       {theme === "dark" ? (
-    //         <Sun className="text-[24px] text-dark-text -mt-1 sm:mt-0" />
-    //       ) : (
-    //         <Moon className="text-[22px] w-[24px] -mt-2 sm:mt-0" />
-    //       )}
-    //     </label>
-    //   )}
-    // </ThemeToggler>
     <>
+      <Helmet
+        bodyAttributes={{
+          class: darkMode.value === true ? "dark" : "light",
+        }}
+      />
       <label {...props}>
         <input
           type="checkbox"
