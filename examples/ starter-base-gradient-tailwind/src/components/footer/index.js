f60fba14d400f2303f6cc1d7b41880825c0da3f8
Alexandra Spalato <alexaspalato@gmail.com>
2021-11-17 16:29:46 +0100

    sticky footer

--- examples/ starter-base-gradient-tailwind/src/components/footer/index.js
+++ examples/ starter-base-gradient-tailwind/src/components/footer/index.js
@@ -6,6 +6,7 @@ export const Footer = ({ ...props }) => {
   return (
     <footer
       className={clsx(
+        "mt-auto",
         "py-5",
         "bg-footerBg dark:bg-dark-footerBg",
         "text-footerColor dark:text-dark-footerColor"
