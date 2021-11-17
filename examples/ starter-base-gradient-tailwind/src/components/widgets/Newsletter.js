95fe75026556ac277fdc7295a5fd1476e5ac99ed
Alexandra Spalato <alexaspalato@gmail.com>
2021-11-17 11:33:27 +0100

    newsletter styling all

--- examples/ starter-base-gradient-tailwind/src/components/widgets/Newsletter.js
+++ examples/ starter-base-gradient-tailwind/src/components/widgets/Newsletter.js
@@ -26,8 +26,6 @@ export const Newsletter = (props) => {
         <>
           <WidgetTitle title="Newsletter" />
           <form
-            maxWidth="400px"
-            mx="auto"
             className=" max-w-[90%] sm:max-w-[400px] lg:mx-auto"
             onSubmit={handleSubmit}
           >
@@ -38,7 +36,7 @@ export const Newsletter = (props) => {
                   name="email"
                   required
                   value={email}
-                  className="w-full h-10 px-5 border-0 rounded-none bg-nlInputBg dark:bg-dark-nlInputBg text-nlColor dark:text-dark-nlColor"
+                  className="w-full h-10 px-5 border-0 rounded-none bg-nlInputBg dark:bg-dark-nlInputBg text-nlColor dark:text-dark-nlColor focus:outline-none focus:ring-1 focus:ring-primary dark:focus:ring-opacity-40 focus:ring-inset"
                   onChange={handleChange}
                   aria-label="Add your Email address to subscribe"
                 />
@@ -46,7 +44,7 @@ export const Newsletter = (props) => {
               <div className="w-1/3">
                 <button
                   type="submit"
-                  className="h-10 px-5 transition duration-500 bg-nlButtonBg dark:bg-dark-nlButtonBg hover:bg-nlButtonHoverBg dark:hover:bg-dark-nlButtonHoverBg"
+                  className="h-10 btn bg-nlButtonBg dark:bg-dark-nlButtonBg dark:text-dark-nlButtonColor hover:bg-nlButtonHoverBg dark:hover:bg-dark-nlButtonHoverBg"
                 >
                   Subscribe
                 </button>
--- examples/starter-naked/src/components/widgets/Newsletter.js
+++ examples/starter-naked/src/components/widgets/Newsletter.js
@@ -26,12 +26,7 @@ export const Newsletter = (props) => {
           <h3 mb={5} className="widget-title">
             Newsletter
           </h3>
-          <form
-            maxWidth="400px"
-            mx="auto"
-            className=" max-w-[400px] lg:mx-auto"
-            onSubmit={handleSubmit}
-          >
+          <form className=" max-w-[400px] lg:mx-auto" onSubmit={handleSubmit}>
             <div className="flex">
               <div className="w-2/3">
                 <input
@@ -39,7 +34,7 @@ export const Newsletter = (props) => {
                   name="email"
                   required
                   value={email}
-                  className="h-10 px-5 w-full border-0 rounded-none bg-nlInputBg dark:bg-dark-nlInputBg text-nlColor dark:text-dark-nlColor"
+                  className="w-full h-10 px-5 border-0 rounded-none bg-nlInputBg dark:bg-dark-nlInputBg text-nlColor dark:text-dark-nlColor focus:outline-none focus:ring-1 focus:ring-primary dark:focus:ring-opacity-40 focus:ring-inset"
                   onChange={handleChange}
                   aria-label="Add your Email address to subscribe"
                 />
@@ -47,7 +42,7 @@ export const Newsletter = (props) => {
               <div className="w-1/3">
                 <button
                   type="submit"
-                  className="btn bg-nlButtonBg dark:bg-dark-nlButtonBg hover:bg-nlButtonHoverBg dark:hover:bg-dark-nlButtonHoverBg"
+                  className="h-10 btn bg-nlButtonBg dark:bg-dark-nlButtonBg dark:text-dark-nlButtonColor hover:bg-nlButtonHoverBg dark:hover:bg-dark-nlButtonHoverBg"
                 >
                   Subscribe
                 </button>
