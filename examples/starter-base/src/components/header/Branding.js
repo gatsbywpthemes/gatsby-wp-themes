e9899d90bb48c890c57639e2b20a51cddc8636d4
alexadark <alexaspalato@gmail.com>
2021-12-14 16:03:48 +0100

    logo

--- examples/starter-base/src/components/header/Branding.js
+++ examples/starter-base/src/components/header/Branding.js
@@ -1,13 +1,21 @@
 import React from "react"
 import { useThemeOptions } from "@gatsbywpthemes/gatsby-theme-blog-data/src/hooks"
 import { Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import ls from "local-storage"
 
 export const Branding = ({ title, ...props }) => {
-  const { logo } = useThemeOptions()
+  const { logo, darkModeLogo } = useThemeOptions()
+  const darkMode = ls.get("darkMode")
+  const img = darkMode && darkModeLogo ? darkModeLogo : logo
+
   return (
     <div className="flex items-center h-full">
       {logo ? (
-        <h3>logo</h3>
+        <GatsbyImage
+          image={img.localFile.childImageSharp.gatsbyImageData}
+          alt="logo"
+        />
       ) : (
         <h1 className="mb-0 text-2xl tracking-wider uppercase" {...props}>
           <Link
