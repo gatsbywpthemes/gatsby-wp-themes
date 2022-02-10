55b98b49d6aef56580b4fe8b5296ceddcef1a132
Alexandra Spalato <alexaspalato@gmail.com>
2022-02-10 11:22:16 +0100

    correction

--- examples/starter-base/src/@gatsbywpthemes/gatsby-theme-blog-data/components/Tag.js
+++ examples/starter-base/src/@gatsbywpthemes/gatsby-theme-blog-data/components/Tag.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Tag from "../../../components/templates/Tag"
+import Tag from "~/components/templates/Tag"
 
 const TagComponent = ({ data, pageContext }) => {
   return (
--- examples/starter-base/src/@gatsbywpthemes/gatsby-theme-blog-data/templates/user-query.js
+++ examples/starter-base/src/@gatsbywpthemes/gatsby-theme-blog-data/templates/user-query.js
@@ -1,5 +1,5 @@
 import { graphql } from "gatsby"
-import User from "../components/User"
+import User from "~/@gatsbywpthemes/gatsby-theme-blog-data/components/User"
 export default User
 
 export const pageQuery = graphql`
