47aa3dc64dd97c5a4b41114b0fd6a6abb27166de
Alexandra Spalato <alexaspalato@gmail.com>
2022-02-27 14:30:20 +0100

    slidemenu

--- examples/starter-test/src/components/header/index.js
+++ examples/starter-test/src/components/header/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Menu } from "~/components/menu"
 import { SearchModal } from "~/components/search/SearchModal"
 import { useThemeOptions } from "@gatsbywpthemes/gatsby-theme-wp-data/src/hooks"
+import { Slidemenu } from "./SlideMenu"
 
 export const Header = () => {
   const { addWordPressSearch: search, addColorModes } = useThemeOptions()
@@ -9,7 +10,8 @@ export const Header = () => {
     <div>
       <h1>Header</h1>
       {search && <SearchModal />}
-      <Menu />
+      <Menu orientation="H" className="hidden md:flex" />
+      <Slidemenu />
     </div>
   )
 }
