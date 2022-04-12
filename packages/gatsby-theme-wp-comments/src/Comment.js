2b39e8666f3f602ad5dc3d7772eefa0e5ae27147
alexadark <alexaspalato@gmail.com>
2022-04-12 12:07:20 +0200

    update apollo

--- packages/gatsby-theme-wp-comments/src/Comment.js
+++ packages/gatsby-theme-wp-comments/src/Comment.js
@@ -1,20 +1,20 @@
-import React, { useContext, memo } from "react"
-import { CommentDate } from "./CommentDate"
-import { CommentForm } from "./CommentForm"
-import { CommentContent } from "./CommentContent"
-import { CommentAuthor } from "./CommentAuthor"
-import { ReplyButton } from "./ReplyButton"
-import { CommentNestingInfo } from "./CommentNestingInfo"
-import { CommentContainer } from "./CommentContainer"
-import { SetActiveCommentContext } from "./context"
+import React, { useContext, memo } from "react";
+import { CommentDate } from "./CommentDate";
+import { CommentForm } from "./CommentForm";
+import { CommentContent } from "./CommentContent";
+import { CommentAuthor } from "./CommentAuthor";
+import { ReplyButton } from "./ReplyButton";
+import { CommentNestingInfo } from "./CommentNestingInfo";
+import { CommentContainer } from "./CommentContainer";
+import { SetActiveCommentContext } from "./context";
 
 export const Comment = memo(
   ({ comment, withReply, activeComment }) => {
-    const { author, date, content, commentId } = comment
-    const setActiveComment = useContext(SetActiveCommentContext)
+    const { author, date, content, commentId } = comment;
+    const setActiveComment = useContext(SetActiveCommentContext);
     return (
       <CommentContainer>
-        <CommentAuthor name={author.node.name} url={author.node.url} />
+        <CommentAuthor name={author?.node?.name} url={author?.node?.url} />
         <CommentDate date={date} />
         <CommentContent content={content} />
         {withReply ? (
@@ -27,11 +27,11 @@ export const Comment = memo(
           <CommentNestingInfo />
         )}
       </CommentContainer>
-    )
+    );
   },
   (prev, next) =>
     (prev.activeComment === prev.comment.commentId &&
       next.activeComment === next.comment.commentId) ||
     (prev.activeComment !== prev.comment.commentId &&
       next.activeComment !== next.comment.commentId)
-)
+);
