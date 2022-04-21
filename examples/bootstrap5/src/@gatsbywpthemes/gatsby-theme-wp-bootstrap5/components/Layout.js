004ba7a3b5d3249ad7cf8e9c26ff93ecbee803e5
alexadark <alexaspalato@gmail.com>
2022-04-21 09:59:40 +0200

    remve bootstrap5 theme

--- examples/bootstrap5/src/@gatsbywpthemes/gatsby-theme-wp-bootstrap5/components/Layout.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "@gatsbywpthemes/gatsby-theme-wp-bootstrap5/src/components/Layout"
-import "../../../styles/style.scss"
-
-export default (props) => (
-  <>
-    <Layout {...props} />
-  </>
-)
