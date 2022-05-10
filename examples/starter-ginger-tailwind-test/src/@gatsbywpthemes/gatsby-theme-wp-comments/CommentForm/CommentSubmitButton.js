f5bb9e0bcc39024677cb361e8d7066a8763b4140
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2022-05-10 18:01:57 +0530

    Comment button design updated to match

--- examples/starter-ginger-tailwind-test/src/@gatsbywpthemes/gatsby-theme-wp-comments/CommentForm/CommentSubmitButton.js
+++ examples/starter-ginger-tailwind-test/src/@gatsbywpthemes/gatsby-theme-wp-comments/CommentForm/CommentSubmitButton.js
@@ -4,7 +4,7 @@ export const CommentSubmitButton = ({ commentStatus }) => {
   return (
     <div className="w-full">
       <button
-        className="flex ml-auto submit-button btn btn-ghost"
+        className="flex ml-auto px-4 py-1 transition duration-300 ease-in-out btn-ghost hover:bg-white hover:!text-dark-bg text-white border-white"
         type="submit"
         disabled={commentStatus === "loading"}
       >
