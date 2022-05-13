db3acbafc47e0f437e4f1b1c90b0c2d8f2575b0a
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2022-05-13 16:34:13 +0530

    Search form in the sidebar updated

--- examples/starter-ginger-tailwind-test/src/@gatsbywpthemes/gatsby-theme-wp-search/SearchForm/SearchInput.js
+++ examples/starter-ginger-tailwind-test/src/@gatsbywpthemes/gatsby-theme-wp-search/SearchForm/SearchInput.js
@@ -1,5 +1,4 @@
 import React from "react"
-import clsx from "clsx"
 
 export const SearchInput = (props) => {
   return (
@@ -7,14 +6,7 @@ export const SearchInput = (props) => {
       css={{
         "::-webkit-search-cancel-button": { WebkitAppearance: "none" },
       }}
-      className={clsx(
-        "search-box",
-        "lg:w-[80%] px-5",
-        "bg-transparent text-text dark:text-dark-text ",
-        "text-3xl",
-        "border-0 border-l-[5px] border-accentColor dark:border-dark-accentColor",
-        "focus:outline-none focus:ring-gray-200 dark:focus:ring-opacity-20 focus:border-0 focus:border-l-[5px] focus:placeholder-transparent focus:border-accentColor dark:focus:border-dark-accentColor"
-      )}
+      className="search-input w-full !px-2 !py-0.5 bg-transparent border-0 border-b focus:outline-none focus:ring-0"
       placeholder="search here..."
       aria-label="Search here"
       {...props}
---
+++ examples/starter-ginger-tailwind-test/src/@gatsbywpthemes/gatsby-theme-wp-search/SearchForm/SubmitButton.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export function SubmitButton() {
+  return (
+    <button
+      className="border-2 uppercase font-bold flex items-center justify-center px-4"
+      type="submit"
+    >
+      Search
+    </button>
+  )
+}
