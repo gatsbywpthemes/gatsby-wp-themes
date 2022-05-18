22c7b796ad4838bf70aa73e559b9bf6826f9ddf4
alexadark <alexaspalato@gmail.com>
2022-05-18 16:14:22 +0200

    lightbox working

--- packages/gatsby-plugin-wordpress-lightbox/src/LightboxWrapper.js
+++ packages/gatsby-plugin-wordpress-lightbox/src/LightboxWrapper.js
@@ -1,14 +1,14 @@
-import React, { useContext } from "react"
-import { LightboxContext } from "./context"
-import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox"
+import React, { useContext } from "react";
+import { LightboxContext } from "./context";
+import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
 
 const LightboxWrapper = ({ children }) => {
-  const options = useContext(LightboxContext)
+  const options = useContext(LightboxContext);
   return (
     <SimpleReactLightbox>
       <SRLWrapper options={{ ...options }}>{children}</SRLWrapper>
     </SimpleReactLightbox>
-  )
-}
+  );
+};
 
-export default LightboxWrapper
+export default LightboxWrapper;
