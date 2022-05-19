f91a880d72930209c2c41a25f37b1864d1323164
alexadark <alexaspalato@gmail.com>
2022-05-19 11:20:52 +0200

    remove lightbox plugin everywhere

--- examples/starter-ginger-kitchen/src/utils/ParsedContent.js
+++ examples/starter-ginger-kitchen/src/utils/ParsedContent.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import ContentParser from '@gatsbywpthemes/gatsby-plugin-wordpress-parser'
 import { cf7ParserFunction } from '@gatsbywpthemes/gatsby-plugin-wpcf7'
-import { lightboxParserFunction } from '@gatsbywpthemes/gatsby-plugin-wordpress-lightbox'
 
 export const ParsedContent = ({ content }) => {
-  const parserFunctions = [cf7ParserFunction, lightboxParserFunction]
+  const parserFunctions = [cf7ParserFunction]
   return <ContentParser content={content} customFn={parserFunctions} />
 }
--- packages/gatsby-theme-wp-base/src/utils/ParsedContent.js
+++ packages/gatsby-theme-wp-base/src/utils/ParsedContent.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ContentParser from '@gatsbywpthemes/gatsby-plugin-wordpress-parser'
 import { cf7ParserFunction } from '@gatsbywpthemes/gatsby-plugin-wpcf7'
-import { lightboxParserFunction } from '@gatsbywpthemes/gatsby-plugin-wordpress-lightbox'
 
 const ParsedContent = ({ content }) => {
-  const parserFunctions = [cf7ParserFunction, lightboxParserFunction]
+  const parserFunctions = [cf7ParserFunction]
   return <ContentParser content={content} customFn={parserFunctions} />
 }
 
