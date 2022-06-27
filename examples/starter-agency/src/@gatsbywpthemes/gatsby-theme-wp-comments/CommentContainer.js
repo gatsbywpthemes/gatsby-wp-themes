9d3b776275378743195acfb2d4857762af2a5319
alexandra spalato <alexaspalato@gmail.com>
2022-06-27 13:07:34 +0200

    comments styles

--- examples/starter-agency/src/@gatsbywpthemes/gatsby-theme-wp-comments/CommentContainer.js
+++ examples/starter-agency/src/@gatsbywpthemes/gatsby-theme-wp-comments/CommentContainer.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export const CommentContainer = ({ children }) => {
-  return <div className="p-4 mb-6 rounded-lg comment card ">{children}</div>
+  return (
+    <div className="p-4 mb-6 transition duration-500 rounded-lg shadow-md comment hover:shadow-lg ">
+      {children}
+    </div>
+  )
 }
